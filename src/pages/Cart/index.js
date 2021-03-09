31f791ad101bfc6fd8869b86206d96026705fd38
Adilson Ramos <75184157+AdilsonRamosO@users.noreply.github.com>
2021-03-08 21:58:48 -0300

    Reducer iniciado

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
-import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from "react-icons/md";
+import { connect } from 'react-redux'
+import { 
+    MdRemoveCircleOutline, 
+    MdAddCircleOutline, 
+    MdDelete 
+} from "react-icons/md";
 
 import { Container, ProductTable, Total } from './styles'
 
-export default function index() {
+function Cart({ cart }) {
     return (
         <Container>
             <ProductTable>
@@ -17,38 +22,40 @@ export default function index() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>
-                            <img 
-                            src="https://static.netshoes.com.br/produtos/tenis-asics-netburner-ballistic-ff-mt-2-masculino/72/D18-6373-172/D18-6373-172_zoom2.jpg?ts=1605025758&ims=326x" 
-                            alt="Tênis"
-                            />
+                    { cart.map(product => (
+                        <tr>
+                            <td>
+                                <img 
+                                src={product.image}
+                                alt={product.title}
+                                />
 
-                        </td>
-                        <td>
-                            <strong>Tênis muito massa</strong>
-                            <span>R$129,90</span>
-                        </td>
-                        <td>
-                            <div>
+                            </td>
+                            <td>
+                                <strong>{product.title}</strong>
+                                <span>{product.priceFormatted}</span>
+                            </td>
+                            <td>
+                                <div>
+                                    <button type="button">
+                                        <MdRemoveCircleOutline size={20} color="#7159c1" />
+                                    </button>
+                                    <input type="number" readOnly value={product.amount} />
+                                    <button type="button">
+                                        <MdAddCircleOutline size={20} color="#7159c1" />
+                                    </button>
+                                </div>
+                            </td>
+                            <td>
+                                <strong>R$258,80</strong>
+                            </td>
+                            <td>
                                 <button type="button">
-                                    <MdRemoveCircleOutline size={20} color="#7159c1" />
+                                    <MdDelete size={20} color="#7159c1" />
                                 </button>
-                                <input type="number" readOnly value={2} />
-                                <button type="button">
-                                    <MdAddCircleOutline size={20} color="#7159c1" />
-                                </button>
-                            </div>
-                        </td>
-                        <td>
-                            <strong>R$258,80</strong>
-                        </td>
-                        <td>
-                            <button type="button">
-                                <MdDelete size={20} color="#7159c1" />
-                            </button>
-                        </td>
-                    </tr>
+                            </td>
+                        </tr>
+                    ))}
                 </tbody>
             </ProductTable>
 
@@ -63,3 +70,9 @@ export default function index() {
         </Container>
     );
 }
+
+const mapStateToProps = state =>({
+    cart: state.cart,
+});
+
+export default connect(mapStateToProps)(Cart);
