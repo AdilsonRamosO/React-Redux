04417a0d7d213bafe214261d6e5f553d3aff9549
Adilson Ramos <75184157+AdilsonRamosO@users.noreply.github.com>
2021-06-01 16:47:02 -0300

    organizando

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import { Router } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { ToastContainer } from 'react-toastify';
+import React from 'react'
+import { Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { ToastContainer } from 'react-toastify'
 
-import './config/ReactotronConfig';
+import './config/ReactotronConfig'
 
-import GlobalStyle from "./styles/global";
-import Header from './components/Header';
-import Routes from './routes';
+import GlobalStyle from './styles/global'
+import Header from './components/Header'
+import Routes from './routes'
 
 import history from './services/history'
-import store from './store';
+import store from './store'
 
 function App() {
   return (
-      <Router history={history}>
-    <Provider store={store}>
+    <Router history={history}>
+      <Provider store={store}>
         <Header />
         <Routes />
         <GlobalStyle />
-        <ToastContainer autoClose={3000}/>
-    </Provider>
-      </Router>
+        <ToastContainer autoClose={3000} />
+      </Provider>
+    </Router>
   )
 }
 
-export default App;
+export default App
